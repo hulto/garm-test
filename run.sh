1aeed9541d65442edc8f7e261eec46369c10342a
root <root@garm-server-2.us-central1-f.c.garm-test.internal>
2024-12-23 23:18:41 +0000

    It works

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run --rm \
-  --name garm \
-  -p 8080:80 \
-  -v ./etc/garm:/etc/garm:rw \
-  -v /var/run/docker.sock:/var/run/docker.sock:rw \
-  ghcr.io/cloudbase/garm:v0.1.4
